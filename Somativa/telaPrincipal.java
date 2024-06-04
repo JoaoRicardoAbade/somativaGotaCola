3b58e90865f0a302035de9465b4849f7c7d0f2d1
Jonas293599 <jonathansprocati5@gmail.com>
2024-06-04 09:27:35 -0300

    1.205(beta)

--- Somativa/telaPrincipal.java
+++ Somativa/telaPrincipal.java
@@ -4,6 +4,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class telaPrincipal extends JFrame {
     private JPanel painelPrincipal;
@@ -14,6 +18,13 @@ public class telaPrincipal extends JFrame {
         setContentPane(painelPrincipal);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
+        String nomeArquivo = "Somativa/Cadastro.txt";
+//        try{
+//            BufferedReader leitor = new BufferedReader(new FileReader(nomeArquivo));
+//
+//        }
+
+
 
         cadastroEmpresaBtn.addActionListener(new ActionListener() {
             @Override
